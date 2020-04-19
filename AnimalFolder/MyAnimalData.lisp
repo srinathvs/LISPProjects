9db64217d05136148d48bbc382eb0059a742a99a
Srinath V.S <srnthvs@gmail.com>
2020-04-18 21:58:13 -0500

    AnimalCommit2

--- AnimalFolder/MyAnimalData.lisp
+++
@@ -1,7 +0,0 @@
-(setq *nodes* 
-'
-((3
-  "does it have a thinner nose, but a thicker tail and a smaller chest when compared to dogs"
-  COYOTE DOG)
- (2 "does it bark" (3) COW) (THING "Is this thing a mammal" (2) LIZARD)))
-(setq *node-count* 3)
